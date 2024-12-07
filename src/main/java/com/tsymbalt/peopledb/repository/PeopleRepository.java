fc6d2f8a4b78916243533a9db2302f82756432ca
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-12-06 22:40:38 -0600

    PeopleDB project

--- src/main/java/com/tsymbalt/peopledb/repository/PeopleRepository.java
+++ src/main/java/com/tsymbalt/peopledb/repository/PeopleRepository.java
@@ -2,7 +2,19 @@ package com.tsymbalt.peopledb.repository;
 
 import com.tsymbalt.peopledb.model.Person;
 
+import java.sql.Connection;
+
 public class PeopleRepository {
+    private Connection connection;
+    public PeopleRepository(Connection connection) {
+        this.connection = connection;
+
+    }
+    public PeopleRepository() {
+
+    }
+
+
     public Person save(Person person) {
         return person;
     }
--- src/test/java/com/tsymbalt/peopledb/repository/PeopleRepositoryTest.java
+++ src/test/java/com/tsymbalt/peopledb/repository/PeopleRepositoryTest.java
@@ -1,24 +1,40 @@
 package com.tsymbalt.peopledb.repository;
 
 import com.tsymbalt.peopledb.model.Person;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PeopleRepositoryTest {
+    private Connection connection;
+    @BeforeEach
+    void setUp() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:h2:/Users/tamaratsymbaliuk/Documents/peopledb");
+    }
 
     @Test
-    public void canSave() {
-        PeopleRepository repo = new PeopleRepository();
+    public void canSaveOnePerson() throws SQLException {
+        PeopleRepository repo = new PeopleRepository(connection);
         Person john = new Person("John", "Smith", ZonedDateTime.of(1980,11,15, 15,15,0,0, ZoneId.of("-6")));
         Person savedPerson = repo.save(john);
         assertThat(savedPerson.getId()).isGreaterThan(0);
-
-
     }
 
+    @Test
+    public void canSaveTwoPeople() {
+        PeopleRepository repo = new PeopleRepository(connection);
+        Person john = new Person("John", "Smith", ZonedDateTime.of(1980,11,15, 15,15,0,0, ZoneId.of("-6")));
+        Person bobby = new Person("Bobby", "Smith", ZonedDateTime.of(1981,12,15, 15,15,0,0, ZoneId.of("-6")));
+        Person savedPerson1 = repo.save(john);
+        Person savedPerson2 = repo.save(john);
+        assertThat(savedPerson1.getId()).isNotEqualTo(savedPerson2.getId());
+    }
 
 }
