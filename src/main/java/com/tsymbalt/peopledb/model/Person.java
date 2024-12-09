7f7673028a0abb5f9934dda23346b72ea0c9f727
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-12-08 23:42:20 -0600

    PeopleDB project

--- src/main/java/com/tsymbalt/peopledb/model/Person.java
+++ src/main/java/com/tsymbalt/peopledb/model/Person.java
@@ -4,7 +4,22 @@ import java.time.ZonedDateTime;
 
 public class Person {
     private Long id;
+    private String firstName;
+    private String lastName;
+    private ZonedDateTime dob;
+
+    public ZonedDateTime getDob() {
+        return dob;
+    }
+
+    public void setDob(ZonedDateTime dob) {
+        this.dob = dob;
+    }
+
     public Person(String firstName, String lastName, ZonedDateTime dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
     }
 
     public Long getId() {
@@ -13,5 +28,18 @@ public class Person {
 
     public void setId(Long id) {
         this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
--- src/main/java/com/tsymbalt/peopledb/repository/PeopleRepository.java
+++ src/main/java/com/tsymbalt/peopledb/repository/PeopleRepository.java
@@ -2,7 +2,8 @@ package com.tsymbalt.peopledb.repository;
 
 import com.tsymbalt.peopledb.model.Person;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.time.ZoneId;
 
 public class PeopleRepository {
     private Connection connection;
@@ -14,8 +15,23 @@ public class PeopleRepository {
 
     }
 
-
     public Person save(Person person) {
+        String sql = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES(?, ?, ?)";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, person.getFirstName());
+            ps.setString(2, person.getLastName());
+            ps.setTimestamp(3, Timestamp.valueOf(person.getDob().withZoneSameInstant(ZoneId.of("+0")).toLocalDateTime()));
+            int recordsAffected = ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            while (rs.next()) {
+                long id = rs.getLong(1);
+                person.setId(id);
+            }
+            System.out.printf("Records affected: %d%n", recordsAffected);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return person;
     }
 }
